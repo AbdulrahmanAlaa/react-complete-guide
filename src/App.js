c433469edbaa87fa78616b4d37f337cb93ae8fc1
abdulrahman alaa <abdulrahman alaa>
2018-02-25 13:37:32 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Person from './Person/Person';
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Abdo', age: 26 },
+      { name: 'Islam', age: 25 },
+      { name: 'Mostafa', age: 28 },
+    ]
+  };
+  swishNameHandler = ()=>{
+    console.log('clicked !!')
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+        <button onClick={this.swishNameHandler}>
+        Swish Names
+        </button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >Some Dynamic Data</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    );
+    //Old Syntax
+    //return React.createElement('div',{className:'App'},React.createElement('h1',null,'Dos This Work?'));
+  }
+}
+
+export default App;

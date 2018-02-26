f470774fab75fe14f5d8fbad019457d4bd7f17e7
abdulrahman alaa <abdulrahman alaa>
2018-02-26 17:44:48 +0200

    apply styling using new modular way on person button

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,24 +3,13 @@ import classes from './Person.css'
 import Radium from 'radium';
 
 const person = (props) => {
-    const styles = {
-        padding:'10px',
-        margin:'10px',
-        border:'1px solid #ccc',
-        boxShadow:'1px 2px 3px #eee',
-        backgroundColor:'#ee5050',
-        fontSize:'12pt',
-        color:'white',
-        '@media (max-width: 500px)':{
-            width: '100%'
-        }
-    }
+    
     return (
         <div className={classes.Person}>
             <p onClick={props.click}>This is a {props.name} and i am {props.age} years old </p>
             <p>{props.children}</p>
             <input type="text" value={props.name} onChange={props.changed} />
-            <button style={styles} className="" onClick={props.click}>Delete</button>
+            <button onClick={props.click}>Delete</button>
         </div>
     );
 

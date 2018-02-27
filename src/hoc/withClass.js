4fb54802ab8bfbd4df27d6128d10a6d6e03a3454
abdulrahman alaa <abdulrahman alaa>
2018-02-28 01:00:57 +0200

    adding higher order component

---
+++ src/hoc/withClass.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const withClass = (WrapperComponent, className)=> {
+    return (props) => <div className={className}><WrapperComponent/></div>
+}
+export default withClass;

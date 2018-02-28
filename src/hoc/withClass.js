168ff9b24d46bbf95dbdf0d398bc5cee80e12823
abdulrahman alaa <abdulrahman alaa>
2018-02-28 11:18:35 +0200

    HOC as class and as pure function

--- src/hoc/withClass.js
+++ src/hoc/withClass.js
@@ -1,5 +1,12 @@
-import React from 'react';
+import React, {Component} from 'react';
+// const withClass = (WrapperComponent, className)=> {
+//     return (props) => <div className={className}><WrapperComponent {...props}/></div>
+// }
 const withClass = (WrapperComponent, className)=> {
-    return (props) => <div className={className}><WrapperComponent/></div>
+    return class extends Component {
+        render(){
+            return <div className={className}><WrapperComponent {...this.props}/></div>;
+        }
+    }
 }
 export default withClass;
